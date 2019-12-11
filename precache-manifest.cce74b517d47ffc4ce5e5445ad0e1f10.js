4f41ec15c2a013589673dfd2247589e48402a213
Hamza Alam <smhamza124@gmail.com>
2019-12-11 23:51:52 +0500

    responsive view build with social link integration

---
+++ precache-manifest.cce74b517d47ffc4ce5e5445ad0e1f10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caee0a9ebd1a3b8aee22a5e3051264ea",
+    "url": "/developer-folio-app/index.html"
+  },
+  {
+    "revision": "afe52722cbd29dc09a26",
+    "url": "/developer-folio-app/static/css/main.3ae2a134.chunk.css"
+  },
+  {
+    "revision": "8023129da09682773e68",
+    "url": "/developer-folio-app/static/js/2.f7c151d1.chunk.js"
+  },
+  {
+    "revision": "afe52722cbd29dc09a26",
+    "url": "/developer-folio-app/static/js/main.c35bc821.chunk.js"
+  },
+  {
+    "revision": "8e955ecce8632c6ad4ac",
+    "url": "/developer-folio-app/static/js/runtime-main.3d1629ef.js"
+  }
+]);
