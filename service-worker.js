7779c1e5590ca908c92737af60a56343a2cdacd6
Hamza Alam <smhamza124@gmail.com>
2020-01-02 00:46:06 +0500

    static deployment

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/developer-folio-app/precache-manifest.f5024046b2dbd4bb9fea2fcb49816183.js"
+  "/developer-folio-app/precache-manifest.561817ba174796c3f1f643b3c7ed501e.js"
 );
 
 self.addEventListener('message', (event) => {
